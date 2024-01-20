6a6278d4eaf4f3ccfad4c056b34d5ea52c10e2ba
Aminur Islam <aminurislam@Aminurs-iMac.local>
2024-01-20 23:04:22 +0600

    markup added on form one

--- templates/user/step-form-one.php
+++ templates/user/step-form-one.php
@@ -4,6 +4,41 @@
 		var_dump( $errors );
 	// }
     ?>
+    <!--login details start-->
+    <div class="login-main-wrapper mt-50 mb-50">
+    <div class="login-container">
+        <div class="login-form-wrap">
+        <div class="login-header">
+            <h4>Login Details</h4>
+            <p>Please enter the necessary information.</p>
+        </div>
+        <!-- form start -->
+        <div class="login-from">
+            <div class="form-group-left">
+            <div class="form-group form-error">
+                <label for="email">E-mail Address</label>
+                <input type="email" placeholder="Enter you mail address" id="email" name="email" class="form-control">
+            </div>
+            <div class="form-group form-error">
+                <label for="password">Password</label>
+                <input type="password" placeholder="Enter your password" id="password" name="password" class="form-control">
+            </div>
+            
+            </div>
+            <div class="form-group-right"></div>
+        </div>
+        <!-- form end -->
+        </div>
+        <div class="form-submit-bttns">
+        <div class="form-submit">
+            <button type="submit" class="btn-submit">Next Step <img src="assets/images/icons/chevron-right.svg"
+                alt="Icon" class="fluid"></button>
+        </div>
+        </div>
+    </div> 
+    </div>
+
+    <!--login details end-->
     <form method="post">
         <?php wp_nonce_field( 'registration-login-details', 'registration-login-details-nonce' ); ?>
         <div class="form-wrapper-box">
