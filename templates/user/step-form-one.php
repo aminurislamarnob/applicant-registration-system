1a84b6bc3a3dfaf410ac36b48784b03184ed043b
Aminur Islam <aminurislam@Aminurs-iMac.local>
2024-01-21 12:03:05 +0600

    form updated with design

--- templates/user/step-form-one.php
+++ templates/user/step-form-one.php
@@ -6,61 +6,41 @@
     ?>
     <!--login details start-->
     <div class="login-main-wrapper mt-50 mb-50">
-    <div class="login-container">
-        <div class="login-form-wrap">
-        <div class="login-header">
-            <h4>Login Details</h4>
-            <p>Please enter the necessary information.</p>
-        </div>
-        <!-- form start -->
-        <div class="login-from">
-            <div class="form-group-left">
-            <div class="form-group form-error">
-                <label for="email">E-mail Address</label>
-                <input type="email" placeholder="Enter you mail address" id="email" name="email" class="form-control">
+        <?php if ( ! is_user_logged_in() ) : ?>
+        <form method="post">
+            <?php wp_nonce_field( 'registration-login-details', 'registration-login-details-nonce' ); ?>
+            <div class="login-container">
+                <div class="login-form-wrap">
+                    <div class="login-header">
+                        <h4><?php echo esc_html__( 'Login Details', 'applicant-registration-system' ); ?></h4>
+                        <p><?php echo esc_html__( 'Please enter the necessary information.', 'applicant-registration-system' ); ?></p>
+                    </div>
+                    <!-- form start -->
+                    <div class="login-from">
+                        <div class="form-group-left">
+                        <div class="form-group form-error">
+                            <label for="user_email"><?php echo esc_html__( 'E-mail Address', 'applicant-registration-system' ); ?></label>
+                            <input type="user_email" placeholder="<?php echo esc_attr__( 'Enter you mail address', 'applicant-registration-system' ); ?>" id="email" name="user_email" class="form-control">
+                        </div>
+                        <div class="form-group form-error">
+                            <label for="user_password"><?php echo esc_html__( 'Password', 'applicant-registration-system' ); ?></label>
+                            <input type="password" placeholder="<?php echo esc_attr__( 'Enter your password', 'applicant-registration-system' ); ?>" id="user_password" name="user_password" class="form-control">
+                        </div>
+                        
+                        </div>
+                        <div class="form-group-right"></div>
+                    </div>
+                    <!-- form end -->
+                </div>
+                <div class="form-submit-bttns">
+                    <div class="form-submit">
+                        <button type="submit" class="btn-submit"><?php echo esc_attr__( 'Next Step', 'applicant-registration-system' ); ?> <img src="<?php echo get_theme_file_uri(); ?>/assets/images/icons/chevron-right.svg" alt="Icon" class="fluid"></button>
+                    </div>
+                </div>
             </div>
-            <div class="form-group form-error">
-                <label for="password">Password</label>
-                <input type="password" placeholder="Enter your password" id="password" name="password" class="form-control">
-            </div>
-            
-            </div>
-            <div class="form-group-right"></div>
-        </div>
-        <!-- form end -->
-        </div>
-        <div class="form-submit-bttns">
-        <div class="form-submit">
-            <button type="submit" class="btn-submit">Next Step <img src="assets/images/icons/chevron-right.svg"
-                alt="Icon" class="fluid"></button>
-        </div>
-        </div>
-    </div> 
+        </form>
+        <?php else : ?>
+        <div class="logged-in-detected"><?php echo esc_html__( 'You are already loggedin.', 'applicant-registration-system' ); ?></div>
+        <?php endif; ?>
     </div>
-
-    <!--login details end-->
-    <form method="post">
-        <?php wp_nonce_field( 'registration-login-details', 'registration-login-details-nonce' ); ?>
-        <div class="form-wrapper-box">
-            <div class="form-step-header">
-                <h2><?php echo esc_html__( 'Login Details', 'applicant-registration-system' ); ?></h2>
-                <p><?php echo esc_html__( 'Please enter the necessary information.', 'applicant-registration-system' ); ?></p>
-            </div>
-            <?php if ( ! is_user_logged_in() ) : ?>
-            <div class="form-field">
-                <label for="user_email"><?php echo esc_html__( 'E-mail Address', 'applicant-registration-system' ); ?></label>
-                <input type="email" name="user_email" id="user_email" placeholder="<?php echo esc_attr__( 'Enter you mail address', 'applicant-registration-system' ); ?>" required>
-            </div>
-            <div class="form-field">
-                <label for="user_password"><?php echo esc_html__( 'Password', 'applicant-registration-system' ); ?></label>
-                <input type="password" name="user_password" id="user_password" placeholder="<?php echo esc_attr__( 'Enter your password', 'applicant-registration-system' ); ?>" required>
-            </div>
-            <?php else : ?>
-            <div class="logged-in-detected"><?php echo esc_html__( 'You are already loggedin.', 'applicant-registration-system' ); ?></div>
-            <?php endif; ?>
-        </div>
-        <div class="form-field form-action-button">
-            <input type="submit" value="<?php echo esc_attr__( 'Next', 'applicant-registration-system' ); ?>">
-        </div>
-    </form>
 </div>
--- templates/user/step-form-two.php
+++ templates/user/step-form-two.php
@@ -1,4 +1,106 @@
 <div class="applicant-step-form">
+<form method="post">
+        <?php wp_nonce_field( 'registration-personal-info', 'registration-personal-info-nonce' ); ?>
+        <!--login details start-->
+    <div class="login-main-wrapper">
+    <div class="login-container">
+        <div class="login-form-wrap">
+        <div class="login-header">
+            <h4><?php echo esc_html__( 'Personal Information', 'applicant-registration-system' ); ?></h4>
+                <p><?php echo esc_html__( 'Please enter the necessary information.', 'applicant-registration-system' ); ?></p>
+        </div>
+        <!-- form start -->
+        <form action="" class="login-from">
+            <div class="personal-info">
+            <div class="form-group-left">
+            <div class="form-group form-error">
+                <label for="first_name"><?php echo esc_html__( 'First Name', 'applicant-registration-system' ); ?></label>
+                <input type="text" placeholder="<?php echo esc_attr__( 'First name', 'applicant-registration-system' ); ?>" id="first_name" name="first_name" class="form-control">
+            </div>
+            <div class="form-group form-error">
+                <label for="date_of_birth"><?php echo esc_html__( 'Date of Birth', 'applicant-registration-system' ); ?></label>
+                <input type="date" placeholder="<?php echo esc_attr__( 'dd-mm-yyyy', 'applicant-registration-system' ); ?>" id="date_of_birth" name="date_of_birth" class="form-control">
+            </div>
+            </div>
+        </div>
+
+            <div class="form-group-right">
+            <div class="form-group form-error">
+                <label for="last_name"><?php echo esc_html__( 'Last Name', 'applicant-registration-system' ); ?></label>
+                <input type="text" placeholder="Last name" id="last_name" name="last_name" class="form-control">
+            </div>
+            <div class="form-group form-error button-option">
+                <div class="form-field">
+                    <label for="have_driving_licence"><?php echo esc_html__( 'Do you have Driving Licence?', 'applicant-registration-system' ); ?></label>
+                    <input type="checkbox" name="have_driving_licence" id="have_driving_licence">
+                </div>
+                <div class="form-field">
+                    <label for="have_car"><?php echo esc_html__( 'Do you have Car?', 'applicant-registration-system' ); ?></label>
+                    <input type="checkbox" name="have_car" id="have_car">
+                </div>
+            </div>
+            </div>
+            <div class="form-group-left">
+            <div class="form-group form-error">
+                <label for="country"><?php echo esc_html__( 'Country', 'applicant-registration-system' ); ?></label>
+                <input type="text" placeholder="<?php echo esc_attr__( 'Country', 'applicant-registration-system' ); ?>" id="country" name="country" class="form-control">
+            </div>
+
+
+            <div class="form-group-row">
+                <div class="form-field">
+                    <label for="zip_code"><?php echo esc_html__( 'Zip Code', 'applicant-registration-system' ); ?></label>
+                    <input type="text" name="zip_code" id="zip_code" placeholder="<?php echo esc_attr__( 'Zip Code', 'applicant-registration-system' ); ?>">
+                </div>
+                <div class="form-field">
+                    <label for="street"><?php echo esc_html__( 'Street', 'applicant-registration-system' ); ?></label>
+                    <input type="text" name="street" id="street" placeholder="<?php echo esc_attr__( 'Street', 'applicant-registration-system' ); ?>">
+                </div>
+            </div>
+
+
+
+            <div class="form-group form-error">
+                <label for="zip_code"><?php echo esc_html__( 'Zip Code', 'applicant-registration-system' ); ?></label>
+                <input type="text" placeholder="<?php echo esc_attr__( 'Zip Code', 'applicant-registration-system' ); ?>" id="zip_code" name="zip_code" class="form-control">
+            </div>
+            <div class="form-group form-error">
+                <label for="apartment">Apartment</label>
+                <input type="text" placeholder="Apartment" id="apartment" name="apartment" class="form-control">
+            </div>
+            <div class="form-group form-error">
+                <label for="number">Phone</label>
+                <input type="number" placeholder="Phone Number" id="number" name="number" class="form-control">
+            </div>
+            </div>
+            <div class="form-group-right">
+            <div class="form-group form-error">
+                <label for="city"><?php echo esc_html__( 'City', 'applicant-registration-system' ); ?></label>
+                <input type="text" placeholder="<?php echo esc_attr__( 'City', 'applicant-registration-system' ); ?>" id="city" name="city" class="form-control">
+            </div>
+            <div class="form-group form-error">
+                <label for="lname"><?php echo esc_html__( 'Street', 'applicant-registration-system' ); ?></label>
+                <input type="text" placeholder="Street" id="lname" name="lname" class="form-control">
+            </div>
+            <div class="form-group form-error">
+                <label for="number">Mobile</label>
+                <input type="number" placeholder="+31612345678" id="number" name="number" class="form-control">
+            </div>
+            </div>
+        </form>
+        <!-- form end -->
+    </div>
+    <div class="form-submit">
+        <button type="submit" class="btn-submit btn-back"><img src="assets/images/icons/chevron-left.svg"alt="Icon" class="fluid">Go back</button>
+        <button type="submit" class="btn-submit">Next Step <img src="assets/images/icons/chevron-right.svg"alt="Icon" class="fluid"></button>  
+    </div>
+    </div> 
+    </div>
+    <!--login details end-->
+</form>
+
+
+
     <form method="post">
         <?php wp_nonce_field( 'registration-personal-info', 'registration-personal-info-nonce' ); ?>
         <div class="form-wrapper-box">
